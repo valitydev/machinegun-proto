178d24216917bf162f94c24b9f7790ef4bba6ccc
Alexey S <kehitt@users.noreply.github.com>
2022-06-10 18:56:36 +0300

    OPS-108: Lifecycle sink proto (#27)

---
+++ proto/lifecycle_sink.thrift
@@ -0,0 +1,48 @@
+namespace erlang mg_lifesink
+namespace java dev.vality.machinegun.lifesink
+
+/**
+ * Определения структур для поддержания взаимодействия с потребителями
+ * событий, порождённых жизненными циклами сущностей machinegun.
+ */
+
+// Loosely inspired by valitydev/machinegun-core/include/pulse.hrl
+
+include "base.thrift"
+include "msgpack.thrift"
+
+union MachineStatus {
+    1: MachineStatusWorking working
+    2: MachineStatusFailed  failed
+}
+
+struct MachineStatusWorking {}
+struct MachineStatusFailed {
+    1: optional string reason
+}
+
+struct LifecycleEvent {
+    1: required base.Namespace     machine_ns     /* Идентификатор пространства имён, породившего событие */
+    2: required base.ID            machine_id     /* Идентификатор машины, породившей событие */
+    3: required base.Timestamp     created_at     /* Время происхождения события */
+    4: required LifecycleEventData data           /* Данные события */
+}
+
+union LifecycleEventData {
+    1: MachineLifecycleEvent    machine
+    // If we want to achieve the goal of mg replication, we can extend this for other things later, e.g.:
+    // 2: TimerLifecycleEvent    timer
+    // ... etc.
+}
+
+union MachineLifecycleEvent {
+    1: MachineLifecycleCreatedEvent       created
+    2: MachineLifecycleStatusChangedEvent status_changed
+    3: MachineLifecycleCreatedEvent       removed
+}
+
+struct MachineLifecycleCreatedEvent {}
+struct MachineLifecycleStatusChangedEvent {
+   1: required MachineStatus new_status
+}
+struct MachineLifecycleRemovedEvent {}
